18c984a27850cb52ffb5b1d71aad05dcfc94f41b
RAX3R <45417916+vqpv@users.noreply.github.com>
2026-07-30 22:11:29 +0300

    3.1 - 8

---
+++ 3. Числа/3.1 Числа 1/8.py
@@ -0,0 +1,6 @@
+r, g, b = 255, 100, 0
+age, weight, height = 24, 70, 175
+line_length, radius, diagonal = 15, 60, 120
+len_of_car = len_of_sofa = len_of_carpet = 1.37
+
+print(r, g, b, age, weight, height, line_length, radius, diagonal, len_of_car, len_of_sofa, len_of_carpet)
