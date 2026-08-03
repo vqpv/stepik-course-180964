56b7052fbbeadd038cbde6b52ca10afb6baa6a21
RAX3R <45417916+vqpv@users.noreply.github.com>
2026-08-03 19:07:42 +0300

    3.2 - 5

---
+++ 3. Числа/3.2 Числа 2/5.py
@@ -0,0 +1,6 @@
+alon = cake_number // 100000
+van_dama = cake_number // 10000 % 10
+stal_one = cake_number // 1000 % 10
+jecki_kachan = cake_number // 100 % 10
+chack_no_rise = cake_number // 10 % 10
+bruslya = cake_number % 10
