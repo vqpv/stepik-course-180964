f0b824a78decdc63fcb436f8356fbe078a0754f7
RAX3R <45417916+vqpv@users.noreply.github.com>
2026-08-17 20:14:20 +0300

    5.1 - 1

---
+++ 5. Списки/5.1 Списки/1.py
@@ -0,0 +1,6 @@
+rating = ["чудовищно", "не фонтан", "середнячок" , "недурно", "отлично"]
+students = ["Dony Jepp", "Mill Buray", "Lorlando Bum"]
+
+print(f"{students[0]} вёл себя сегодня {rating[0]}")
+print(f"{students[1]} вёл себя сегодня {rating[1]}")
+print(f"{students[2]} вёл себя сегодня {rating[3]}")
