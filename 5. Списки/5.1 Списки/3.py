e13a772e49ee8f559a0eb9d97604fe564f6d767b
RAX3R <45417916+vqpv@users.noreply.github.com>
2026-08-19 12:20:44 +0300

    5.1 - 3

---
+++ 5. Списки/5.1 Списки/3.py
@@ -0,0 +1,7 @@
+print(f"""Имя: {result[0]}
+Предмет: {result[1]}
+Количество 1: {result.count(1)}
+Количество 2: {result.count(2)}
+Количество 3: {result.count(3)}
+Количество 4: {result.count(4)}
+Количество 5: {result.count(5)}""")
