f5c1b79cdb4a38d417d714bfccea8487bcccacea
RAX3R <45417916+vqpv@users.noreply.github.com>
2026-08-21 19:28:22 +0300

    5.1 - 6

---
+++ 5. Списки/5.1 Списки/6.py
@@ -0,0 +1,11 @@
+student = []
+
+student.append(["Политология"])
+student.append(["Физкультура"])
+student.append(["География"])
+
+student[0].extend([5, 5, 5])
+student[1].extend([3, 3, 3])
+student[2].extend([4, 4, 4])
+
+print(student)
