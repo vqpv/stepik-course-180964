ecfbc6e7120901e7b592f4a554965eb65d1ec347
RAX3R <45417916+vqpv@users.noreply.github.com>
2026-08-30 23:33:15 +0300

    7.1 - 3

---
+++ 7. Условия и Проверки/7.1 Условные и логические операторы/3.py
@@ -0,0 +1,12 @@
+garbage = input()
+
+if garbage == "металл":
+    print("жёлтый")
+elif garbage == "бумага":
+    print("синий")
+elif garbage == "стекло":
+    print("зелёный")
+elif garbage == "пластик":
+    print("оранжевый")
+else:
+    print("ручная сортировка")
