a96ecaf322db6eba5d25e730096fc6cf08ea8733
RAX3R <45417916+vqpv@users.noreply.github.com>
2026-09-11 10:12:43 +0300

    8.2 - 2

---
+++ 8. Циклы/8.2 Итератор, функции и for/2.py
@@ -0,0 +1,10 @@
+my_list = [10, 20, 30, 40]
+my_iterator = iter(my_list)
+print(next(my_iterator))  
+print(next(my_iterator)) 
+
+my_iterator_2 = iter(my_list)
+print(next(my_iterator_2))  
+print(next(my_iterator_2))
+print(next(my_iterator_2))  
+print(next(my_iterator_2))
